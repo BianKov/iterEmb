417db01512cd9bbb058ce3582892653123976655
Sadamori Kojaku <skojaku@iu.edu>
2023-06-02 17:19:51 -0400

    make iteremb as a package

---
+++ iteremb/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Author: Sadamori Kojaku
+# @Date:   2023-06-02 16:53:58
+# @Last Modified by:   Sadamori Kojaku
+# @Last Modified time: 2023-06-02 16:58:06
+from .utils import choosingDim
