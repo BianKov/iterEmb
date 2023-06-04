a1a658d578dfa461761cad1ae1dcbe0c0319c89b
Sadamori Kojaku <skojaku@iu.edu>
2023-06-04 06:19:30 -0400

    split embedding and edge weighting

---
+++ iteremb/edge_weighting.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+# @Author: Sadamori Kojaku
+# @Date:   2023-06-03 22:01:20
+# @Last Modified by:   Sadamori Kojaku
+# @Last Modified time: 2023-06-03 22:26:44
+import numpy as np
+from scipy import sparse
+
+weighting_models = {}
+weighting_models = lambda f: weighting_models.setdefault(f.__name__, f)
+
+
+@weighting_model
+def cosine_similarity(
+    A: sparse.csr_matrix, emb: np.ndarray, **params: Any
+) -> sparse.csr_matrix:
+    """
+    Calculate the cosine similarity between embeddings and return a sparse matrix.
+
+    Parameters
+    ----------
+    A : csr_matrix
+        The adjacency matrix of a graph.
+    emb : ndarray
+        The node embedding matrix with shape (num_nodes, dim).
+    **params : dict
+        Additional parameters for future customization.
+
+    Returns
+    -------
+    csr_matrix
+        A sparse matrix representing the cosine similarity between embeddings.
+    """
+    nemb = np.einsum("ij,i->ij", emb, 1 / np.linalg.norm(emb, axis=1))
+    src, trg, _ = sparse.find(A)
+    w = np.array(np.sum(nemb[src] * nemb[trg], axis=1)).reshape(-1)
+    w = np.clip(w, -1.0, 1.0)
+    return sparse.csr_matrix((w, (src, trg)), shape=A.shape)
+
+
+@weighting_model
+def cosine_distance(A, emb, **params):
+    """
+    Computes the cosine distance between each pair of items in sparse matrix A using embeddings.
+
+    Parameters:
+    -----------
+    A : sparse matrix
+        Sparse matrix representing the user-item interactions.
+    emb : array-like of shape (n_items, n_dimensions)
+        Embedding vectors for each item.
+    **params : dict
+        Dictionary containing additional optional parameters.
+
+    Returns:
+    --------
+    distance_matrix: csr_matrix
+        Sparse cosine distance matrix between each pair of items in A.
+
+    """
+    nemb = np.einsum("ij,i->ij", emb, 1 / np.linalg.norm(emb, axis=1))
+    src, trg, _ = sparse.find(A)
+    w = 1.0 - np.array(np.sum(nemb[src] * nemb[trg], axis=1)).reshape(-1)
+    w = np.clip(w, 0.0, 2.0)
+    return sparse.csr_matrix((w, (src, trg)), shape=A.shape)
+
+
+@weighting_model
+def exp_cosine_similarity(A, emb, q):
+    """
+    Computes the exponential cosine similarity between each pair of items in sparse matrix A using embeddings.
+
+    Parameters:
+    -----------
+    A : sparse matrix
+        Sparse matrix representing the user-item interactions.
+    emb : array-like of shape (n_items, n_dimensions)
+        Embedding vectors for each item.
+    q : float
+        Exponential decay parameter.
+
+    Returns:
+    --------
+    similarity_matrix : csr_matrix
+        Sparse exponential cosine similarity matrix between each pair of items in A.
+    """
+    nemb = np.einsum("ij,i->ij", emb, 1 / np.linalg.norm(emb, axis=1))
+    src, trg, _ = sparse.find(A)
+    w = np.array(np.sum(nemb[src] * nemb[trg], axis=1)).reshape(-1) - 1.0
+    w = np.clip(w, -2.0, 0.0)
+    w = np.exp(q * w)
+    return sparse.csr_matrix((w, (src, trg)), shape=A.shape)
